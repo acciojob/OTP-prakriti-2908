dd5130cf8de31d4717dd21a9a91f01477fbd3da8
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-17 20:55:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,6 @@
 //your JS code here. If required.
-const inputs=document.querySelectorAll("input")
-
-inputs.forEach((input,index)=>
-	{
-	input.addEventListener("keyup",(eventdetails)=>{
-		
-	 const curInput = input;
-     const next = input.nextElementSibling;
-     const prev = input.previousElementSibling;
-
-	if(next && curInput.value!=="")
-	{
-		next.focus();
-	}
-		if(eventdetails.key ==="Backspace")
-		{
-			inputs.forEach((input,index2)=>{
-				if(index<=index2 && prev)
-				{
-			        curInput.value="";
-					prev.focus()
-				}
-				
-			})
-			
-		}
-	})
-})
-window.addEventListener("load",()=>inputs[0].focus());
+function focusNext(current, nextFieldID) {
+    if (current.value.length === current.maxLength) {
+        document.getElementById(nextFieldID).focus();
+    }
+}
